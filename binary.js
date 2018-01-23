b83b358dd242d2320a951cd2222785b55fb8634e
AbhilashJN <abhilashjn107@gmail.com>
2018-01-23 14:59:00 +0530

    test case with 1 number and 1 string passed

--- binary.js
+++ binary.js
@@ -1,4 +1,10 @@
 let binary = (first,second) => {	
-	return first+second;
+	if(typeof(first)==='number' && typeof(second)==='number')
+	{
+		return first+second;
+	}
+	else{
+		return null;
+	}
 };
 module.exports = binary;
--- binary.test.js
+++ binary.test.js
@@ -3,6 +3,7 @@ let binary = require('./binary');
 describe('Testing the binary function' , ()=>
 {
 	test('Testing for number,number argument should return the sum',()=>{expect(binary(1,2)).toBe(3);});
+	test('Testing for number,string argument should return the sum',()=>{expect(binary(1,'2')).toBe(null);});
 
 	
 
