bc7c7946bb8c3cfaf1cb360ed595cbb1e9825840
AbhilashJN <abhilashjn107@gmail.com>
2018-01-23 15:12:34 +0530

    added test case for 1positive and 1 negative number, added test case for no arguments

--- binary.test.js
+++ binary.test.js
@@ -3,8 +3,10 @@ let binary = require('./binary');
 describe('Testing the binary function' , ()=>
 {
 	test('Testing for number,number argument should return the sum',()=>{expect(binary(1,2)).toBe(3);});
+	test('Testing for number,number argument should return the sum',()=>{expect(binary(1,-2)).toBe(-1);});
 	test('Testing for number,string argument should return null',()=>{expect(binary(1,'2')).toBe(null);});
 	test('Testing for number,infinity argument should return null',()=>{expect(binary(1,Infinity)).toBe(null);});
+	test('Testing for no arguments should return null',()=>{expect(binary()).toBe(null);});
 
 	
 
