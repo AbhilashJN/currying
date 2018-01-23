e17ea83958e1fdd9826e033f061eb05edc9a2cab
AbhilashJN <abhilashjn107@gmail.com>
2018-01-24 00:47:26 +0530

    test case for caller passed with empty obj

--- callAndApply.test.js
+++ callAndApply.test.js
@@ -8,5 +8,9 @@ describe('Tests for call function' , ()=>{
 		expect(callAndApply.caller({name:'',age:0,tshirt:''},function(name,age,tshirt){this.name=name ; this.age=age;this.tshirt=tshirt;return this;},'ad',12,'s')).toEqual({name:'ad',age:12,tshirt:'s'});}
 	);
 
-    
+   
+	test('testing with sample update function' , ()=>{
+        
+		expect(callAndApply.caller({},function(name,age,tshirt){this.name=name ; this.age=age;this.tshirt=tshirt;return this;},'ad',12,'s')).toEqual({name:'ad',age:12,tshirt:'s'});}
+	);
 });
